e77f133659f4048bba21ea96aaf09ff8db1b8d04
Emily Dong <emily@northone.com>
2020-05-21 01:54:34 -0400

    add endpoint to add email

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ app.get("/", (req, res) => res.status(201).send())
 app.listen(port, () =>
   console.log(`Example listening at http://localhost:${port}`)
 )
+
+app.post("/emails", async (req, res) => {
+  try {
+    if (req.body.email) {
+      await storeNewEmail(req.body.email)
+      await sendWelcomeEmail(req.body.email)
+    }
+    res.status(201).send()
+  } catch (e) {
+    res.status(500).send()
+  }
+})
